426c8fb37040238ba8431525f677f3675b13ac1f
Horvath9 <pedro.montemurrog1@gmail.com>
2022-06-06 15:19:49 -0300

    efhnfdhn

--- 2. Banco de Dados/ddl.sql
+++ 2. Banco de Dados/ddl.sql
@@ -5,3 +5,4 @@ create table tb_anime(
 id_anime int primary key auto_increment,
 nm_anime varchar(50)
 );
+
--- 2. Banco de Dados/dml.sql
+++ 2. Banco de Dados/dml.sql
@@ -1,3 +1,10 @@
-INSERT INTO tb_anime (nm_anime)
-       VALUES ("blaech")
+-- adicionar animke
+INSERT INTO    tb_anime (id_anime, nm_anime)
+    VALUES (1, 'One Piece');
 
+
+
+    -- listaranime
+    SELECT id_anime     id,
+            nm_anime    nome
+    FROM     tb_anime;
